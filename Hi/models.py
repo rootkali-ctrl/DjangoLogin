113170d57404f52e1e3bf5bc89f9757a75b2fbda
Sri Jaya Karti <64243532+rootkali-ctrl@users.noreply.github.com>
2023-11-23 12:06:06 +0530

    Delete Hi directory

--- Hi/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.username
